6676dc898c12c7dff99c73966fa7a5be9f0eba05
Sangwon Lee <new970628@naver.com>
2024-10-15 06:03:17 +0000

    C5: modify hello.java

---
+++ hello.java
@@ -0,0 +1,7 @@
+public class Hello {
+    public static void main(String[] args) {
+        system.out.println("Hello world from GIT!");
+        system.out.println("Changes after First commit!");
+        system.out.println("Changes after pushing to remote!");
+    }
+}
